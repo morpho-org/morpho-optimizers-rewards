1ee4482bb3efbbd9978799c288c3b68bd2d8033b
Julien <julien@morpho.xyz>
2022-10-05 10:58:47 -0400

    refactor(script): move getUserRewards script

--- src/scripts/getUserRewards.ts
+++ src/scripts/getUserRewards.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { getUserRewards } from "../../../utils";
+import { getUserRewards } from "../utils";
 import { formatUnits } from "ethers/lib/utils";
 
 getUserRewards(process.argv[2], process.argv[3] ? +process.argv[3] : undefined)
