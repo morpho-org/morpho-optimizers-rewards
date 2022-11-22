0b3b6aaa73dfbf98b8db098e39ecc000c02cf5d4
Julien-devatom <julien@morpho.xyz>
2022-11-22 19:07:10 +0100

    test(vaults): withdraws redistribute MORPHO

--- test/vaults/vaults.test.ts
+++ test/vaults/vaults.test.ts
@@ -206,4 +206,106 @@ describe("Vaults", () => {
     console.log(totalDistributed.toString(), totalVaultRewards.toString());
     expectBNApproxEquals(totalDistributed, totalVaultRewards, 10);
   });
+  it("Should distribute a part of MORPHO when user withdraws", async () => {
+    const proofsFetcher = new ProofsFetcher();
+    const allProofs = getAllProofs();
+    const firstProof = allProofs[allProofs.length - 1];
+    const epochConfig = proofsFetcher.getEpochFromId(firstProof.epoch);
+    const distributor = distributorFromEvents("0x00e043300ebebd0f68e1373cc2167eebdb21516c", [
+      {
+        type: VaultEventType.Deposit,
+        event: {
+          blockNumber: epochConfig.initialBlock! + 1000,
+          transactionIndex: 1,
+          logIndex: 1,
+          args: {
+            caller: constants.AddressZero,
+            owner: constants.AddressZero,
+            assets: parseUnits("1000"),
+            shares: parseUnits("1000"),
+          },
+        },
+      },
+      {
+        type: VaultEventType.Deposit,
+        event: {
+          blockNumber: epochConfig.initialBlock! + 2000,
+          transactionIndex: 1,
+          logIndex: 1,
+          args: {
+            caller: "0x0000000000000000000000000000000000000001",
+            owner: "0x0000000000000000000000000000000000000001",
+            assets: parseUnits("1000"),
+            shares: parseUnits("1000"),
+          },
+        },
+      },
+      {
+        type: VaultEventType.Withdraw,
+        event: {
+          blockNumber: epochConfig.initialBlock! + 3000,
+          transactionIndex: 1,
+          logIndex: 2,
+          args: {
+            caller: "0x0000000000000000000000000000000000000001",
+            receiver: "0x0000000000000000000000000000000000000001",
+            owner: "0x0000000000000000000000000000000000000001",
+            assets: parseUnits("1200"),
+            shares: parseUnits("1000"),
+          },
+        },
+      },
+    ]);
+    const merkleTree = await distributor.distributeMorpho(epochConfig.id);
+    expect(merkleTree).toBeDefined();
+    expect(merkleTree.proofs[constants.AddressZero].amount).toBeDefined();
+    expect(merkleTree.proofs["0x0000000000000000000000000000000000000001"].amount).toBeDefined();
+    const totalDistributed = Object.values(merkleTree.proofs).reduce(
+      (acc, proof) => acc.add(proof.amount),
+      BigNumber.from(0)
+    );
+    const totalVaultRewards = BigNumber.from(firstProof.proofs["0x00e043300ebebd0f68e1373cc2167eebdb21516c"]!.amount);
+
+    expect(totalDistributed.lte(totalVaultRewards)).toBeTruthy();
+    console.log(totalDistributed.toString(), totalVaultRewards.toString());
+    expectBNApproxEquals(totalDistributed, totalVaultRewards, 10);
+
+    const distributorWithoutWithdrawal = distributorFromEvents("0x00e043300ebebd0f68e1373cc2167eebdb21516c", [
+      {
+        type: VaultEventType.Deposit,
+        event: {
+          blockNumber: epochConfig.initialBlock! + 1000,
+          transactionIndex: 1,
+          logIndex: 1,
+          args: {
+            caller: constants.AddressZero,
+            owner: constants.AddressZero,
+            assets: parseUnits("1000"),
+            shares: parseUnits("1000"),
+          },
+        },
+      },
+      {
+        type: VaultEventType.Deposit,
+        event: {
+          blockNumber: epochConfig.initialBlock! + 2000,
+          transactionIndex: 1,
+          logIndex: 1,
+          args: {
+            caller: "0x0000000000000000000000000000000000000001",
+            owner: "0x0000000000000000000000000000000000000001",
+            assets: parseUnits("1000"),
+            shares: parseUnits("1000"),
+          },
+        },
+      },
+    ]);
+    const merkleTreeWithoutWithdrawal = await distributorWithoutWithdrawal.distributeMorpho(epochConfig.id);
+    const withoutWithdrawalAmount = BigNumber.from(
+      merkleTreeWithoutWithdrawal.proofs["0x0000000000000000000000000000000000000001"]!.amount
+    );
+    expect(+withoutWithdrawalAmount.toString()).toBeGreaterThan(
+      +merkleTree.proofs["0x0000000000000000000000000000000000000001"]!.amount
+    );
+  });
 });
