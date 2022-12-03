36de4d554682b26f959e14f0f7029f83de4ec985
Julien-devatom <julien@morpho.xyz>
2022-12-03 01:39:40 +0100

    feat(vaults): add Morpho-Compound vaults

--- src/vaults/script/configuration.ts
+++ src/vaults/script/configuration.ts
@@ -29,6 +29,34 @@ const configuration = {
       address: "0x36f8d0d0573ae92326827c4a82fe4ce4c244cab6", // m-aave DAI SV
       deploymentBlock: 15689991,
     },
+    {
+      address: "0xf31ac95fe692190b9c67112d8c912ba9973944f2", // m-compound WBTC SV
+      deploymentBlock: 15732696,
+    },
+    {
+      address: "0xc2a4fba93d4120d304c94e4fd986e0f9d213ed8a", // m-compound USDT SV
+      deploymentBlock: 15732696,
+    },
+    {
+      address: "0xba9e3b3b684719f80657af1a19debc3c772494a0", // m-compound USDC SV
+      deploymentBlock: 15732696,
+    },
+    {
+      address: "0x496da625c736a2ff122638dc26dcf1bfdef1778c", // m-compound UNI SV
+      deploymentBlock: 15732695,
+    },
+    {
+      address: "0xaa768b85ec827ccc36d882c1814bcd27ec4a8593", // m-compound COMP SV
+      deploymentBlock: 15732695,
+    },
+    {
+      address: "0x676e1b7d5856f4f69e10399685e17c2299370e95", // m-compound WETH SV
+      deploymentBlock: 15732695,
+    },
+    {
+      address: "0x8f88eae3e1c01d60bccdc3db3cbd5362dd55d707", // m-compound DAI SV
+      deploymentBlock: 15732694,
+    },
   ],
 };
 export default configuration;
