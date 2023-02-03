62dbc88f86f03b44cd0947353c88fde6ce82178f
juien <julien@morpho.xyz>
2023-02-03 10:18:56 +0100

    feat: clean code

--- src/scripts/updateAgesData.ts
+++ src/scripts/updateAgesData.ts
@@ -6,23 +6,31 @@ import * as path from "path";
 
 const updateAgesData = async () => {
   const changes = await syncAgeConfig();
+
   const agesDataPath = path.resolve(__dirname, "../ages/ages.data.json");
+
   const resulting = ages.map((age) => {
     const epochs = age.epochs.map((epoch) => {
       const epochChanges = changes.filter((c) => c.epoch === epoch.id);
+
       const allValues = epochChanges.reduce((acc, change) => {
         const parsed = +change.value;
         const value = isNaN(parsed) ? change.value : parsed;
         return { ...acc, [change.variable]: value };
       }, {});
+
       if (epochChanges.length > 0) console.log(`Updating ${epoch.id}, ${JSON.stringify(allValues)}`);
       return { ...epoch, ...allValues };
     });
     return { ...age, epochs };
   });
+
   if (changes.length === 0) return console.log("Nothing to update, stopping");
+
   const data = JSON.stringify(resulting, null, 2);
+
   console.log("Writing results to ages.data.json");
+
   await fs.writeFile(agesDataPath, data);
 };
 
