c17500afd220c61305e92a324e321e47f86235db
julien <julien@morpho.xyz>
2023-02-07 18:11:19 +0100

    fix(distributionV2): skip accounting on position update for V1

--- subgraph/src/balances.ts
+++ subgraph/src/balances.ts
@@ -25,22 +25,26 @@ const computeSupplyIndexUpdate = (
   blockTimestamp: BigInt,
   indexUnits: u8,
   balance: Balance,
-  market: Market
+  market: Market,
+  skipV1Accounting: boolean
 ): void => {
-  const newSupplyIndex = updateSupplyIndex(marketAddress, blockTimestamp);
-
   const newSupplyIndexOnPool = updateSupplyIndexOnPool(marketAddress, blockTimestamp, indexUnits);
   const newSupplyIndexInP2P = updateSupplyIndexInP2P(marketAddress, blockTimestamp, indexUnits);
+
+  const newSupplyIndex = updateSupplyIndex(marketAddress, blockTimestamp);
 
   const accumulatedMorphoV1 = accrueMorphoTokens(
     newSupplyIndex,
     balance.userSupplyIndex,
     balance.underlyingSupplyBalance
   );
+  if (!skipV1Accounting) {
+    balance.userSupplyIndex = newSupplyIndex;
+    market.supplyIndex = newSupplyIndex;
 
-  balance.userSupplyIndex = newSupplyIndex;
-
-  balance.accumulatedSupplyMorphoV1 = balance.accumulatedSupplyMorphoV1.plus(accumulatedMorphoV1);
+    balance.accumulatedSupplyMorphoV1 = balance.accumulatedSupplyMorphoV1.plus(accumulatedMorphoV1);
+    market.supplyUpdateBlockTimestampV1 = blockTimestamp;
+  }
 
   const accumulatedMorphoV2 = accrueMorphoTokens(
     newSupplyIndexOnPool,
@@ -50,14 +54,12 @@ const computeSupplyIndexUpdate = (
 
   balance.accumulatedSupplyMorphoV2 = balance.accumulatedSupplyMorphoV2.plus(accumulatedMorphoV2);
   if (blockTimestamp.le(VERSION_2_TIMESTAMP)) {
-    balance.accumulatedSupplyMorpho = balance.accumulatedSupplyMorpho.plus(accumulatedMorphoV1);
+    if (!skipV1Accounting) balance.accumulatedSupplyMorpho = balance.accumulatedSupplyMorpho.plus(accumulatedMorphoV1);
   } else {
     balance.accumulatedSupplyMorpho = balance.accumulatedSupplyMorpho.plus(accumulatedMorphoV2);
   }
   balance.userSupplyOnPoolIndex = newSupplyIndexOnPool;
   balance.userSupplyInP2PIndex = newSupplyIndexInP2P;
-
-  market.supplyIndex = newSupplyIndex;
 
   market.poolSupplyIndex = newSupplyIndexOnPool;
   market.p2pSupplyIndex = newSupplyIndexInP2P;
@@ -74,33 +76,36 @@ export const updateSupplyBalanceAndMarket = (
   blockTimestamp: BigInt,
   newBalanceP2P: BigInt,
   newBalanceOnPool: BigInt,
-  indexUnits: u8
+  indexUnits: u8,
+  skipV1Accounting: boolean
 ): Balance => {
   const market = getOrInitMarket(marketAddress, blockTimestamp);
   const balance = getOrInitBalance(userAddress, marketAddress, blockTimestamp);
   if (market.supplyUpdateBlockTimestamp.lt(VERSION_2_TIMESTAMP) && blockTimestamp.ge(VERSION_2_TIMESTAMP)) {
-    computeSupplyIndexUpdate(marketAddress, VERSION_2_TIMESTAMP, indexUnits, balance, market);
+    computeSupplyIndexUpdate(marketAddress, VERSION_2_TIMESTAMP, indexUnits, balance, market, false);
   }
-  computeSupplyIndexUpdate(marketAddress, blockTimestamp, indexUnits, balance, market);
+  computeSupplyIndexUpdate(marketAddress, blockTimestamp, indexUnits, balance, market, skipV1Accounting);
   // accounting
 
   // V1
-  const newUnderlyingSupplyBalance = newBalanceP2P
-    .times(market.lastP2PSupplyIndex)
-    .plus(newBalanceOnPool.times(market.lastPoolSupplyIndex))
-    .div(WAD);
-  market.lastTotalSupply = market.lastTotalSupply
-    .minus(balance.underlyingSupplyBalance)
-    .plus(newUnderlyingSupplyBalance);
 
+  if (!skipV1Accounting) {
+    const newUnderlyingSupplyBalance = newBalanceP2P
+      .times(market.lastP2PSupplyIndex)
+      .plus(newBalanceOnPool.times(market.lastPoolSupplyIndex))
+      .div(WAD);
+
+    market.lastTotalSupply = market.lastTotalSupply
+      .minus(balance.underlyingSupplyBalance)
+      .plus(newUnderlyingSupplyBalance);
+
+    balance.underlyingSupplyBalance = newUnderlyingSupplyBalance;
+  }
   // V2
   market.scaledSupplyInP2P = market.scaledSupplyInP2P.plus(newBalanceP2P).minus(balance.scaledSupplyInP2P);
   market.scaledSupplyOnPool = market.scaledSupplyOnPool.plus(newBalanceOnPool).minus(balance.scaledSupplyOnPool);
 
   market.save();
-
-  // V1
-  balance.underlyingSupplyBalance = newUnderlyingSupplyBalance;
 
   // V2
   balance.scaledSupplyOnPool = newBalanceOnPool;
@@ -115,7 +120,7 @@ export const updateSupplyBalanceAndMarket = (
 };
 
 /**
- * blockTimestamp & supplyUpdateBlockTimestamp must be in
+ * blockTimestamp & borrowUpdateBlockTimestamp must be in
  * the same version to distribute rewards correctly
  */
 const computeBorrowIndexUpdate = (
@@ -123,7 +128,8 @@ const computeBorrowIndexUpdate = (
   blockTimestamp: BigInt,
   indexUnits: u8,
   balance: Balance,
-  market: Market
+  market: Market,
+  skipV1Accounting: boolean
 ): void => {
   const newBorrowIndex = updateBorrowIndex(marketAddress, blockTimestamp);
 
@@ -135,10 +141,12 @@ const computeBorrowIndexUpdate = (
     balance.userBorrowIndex,
     balance.underlyingBorrowBalance
   );
-
-  balance.userBorrowIndex = newBorrowIndex;
-
-  balance.accumulatedBorrowMorphoV1 = balance.accumulatedBorrowMorphoV1.plus(accumulatedMorphoV1);
+  if (!skipV1Accounting) {
+    balance.userBorrowIndex = newBorrowIndex;
+    balance.accumulatedBorrowMorphoV1 = balance.accumulatedBorrowMorphoV1.plus(accumulatedMorphoV1);
+    market.borrowIndex = newBorrowIndex;
+    market.borrowUpdateBlockTimestampV1 = blockTimestamp;
+  }
 
   const accumulatedMorphoV2 = accrueMorphoTokens(
     newBorrowIndexOnPool,
@@ -148,14 +156,12 @@ const computeBorrowIndexUpdate = (
 
   balance.accumulatedBorrowMorphoV2 = balance.accumulatedBorrowMorphoV2.plus(accumulatedMorphoV2);
   if (blockTimestamp.le(VERSION_2_TIMESTAMP)) {
-    balance.accumulatedBorrowMorpho = balance.accumulatedBorrowMorpho.plus(accumulatedMorphoV1);
+    if (!skipV1Accounting) balance.accumulatedBorrowMorpho = balance.accumulatedBorrowMorpho.plus(accumulatedMorphoV1);
   } else {
     balance.accumulatedBorrowMorpho = balance.accumulatedBorrowMorpho.plus(accumulatedMorphoV2);
   }
   balance.userBorrowOnPoolIndex = newBorrowIndexOnPool;
   balance.userBorrowInP2PIndex = newBorrowIndexInP2P;
-
-  market.borrowIndex = newBorrowIndex;
 
   market.poolBorrowIndex = newBorrowIndexOnPool;
   market.p2pBorrowIndex = newBorrowIndexInP2P;
@@ -171,33 +177,34 @@ export const updateBorrowBalanceAndMarket = (
   blockTimestamp: BigInt,
   newBalanceP2P: BigInt,
   newBalanceOnPool: BigInt,
-  indexUnits: u8
+  indexUnits: u8,
+  skipV1Accounting: boolean
 ): Balance => {
   const market = getOrInitMarket(marketAddress, blockTimestamp);
   const balance = getOrInitBalance(userAddress, marketAddress, blockTimestamp);
   if (market.borrowUpdateBlockTimestamp.lt(VERSION_2_TIMESTAMP) && blockTimestamp.ge(VERSION_2_TIMESTAMP)) {
-    computeBorrowIndexUpdate(marketAddress, VERSION_2_TIMESTAMP, indexUnits, balance, market);
+    computeBorrowIndexUpdate(marketAddress, VERSION_2_TIMESTAMP, indexUnits, balance, market, false);
   }
-  computeBorrowIndexUpdate(marketAddress, blockTimestamp, indexUnits, balance, market);
+  computeBorrowIndexUpdate(marketAddress, blockTimestamp, indexUnits, balance, market, skipV1Accounting);
   // accounting
 
   // V1
-  const newUnderlyingBorrowBalance = newBalanceP2P
-    .times(market.lastP2PBorrowIndex)
-    .plus(newBalanceOnPool.times(market.lastPoolBorrowIndex))
-    .div(WAD);
-  market.lastTotalBorrow = market.lastTotalBorrow
-    .minus(balance.underlyingBorrowBalance)
-    .plus(newUnderlyingBorrowBalance);
-
+  if (!skipV1Accounting) {
+    const newUnderlyingBorrowBalance = newBalanceP2P
+      .times(market.lastP2PBorrowIndex)
+      .plus(newBalanceOnPool.times(market.lastPoolBorrowIndex))
+      .div(WAD);
+    market.lastTotalBorrow = market.lastTotalBorrow
+      .minus(balance.underlyingBorrowBalance)
+      .plus(newUnderlyingBorrowBalance);
+    // V1
+    balance.underlyingBorrowBalance = newUnderlyingBorrowBalance;
+  }
   // V2
   market.scaledBorrowInP2P = market.scaledBorrowInP2P.plus(newBalanceP2P).minus(balance.scaledBorrowInP2P);
   market.scaledBorrowOnPool = market.scaledBorrowOnPool.plus(newBalanceOnPool).minus(balance.scaledBorrowOnPool);
 
   market.save();
-
-  // V1
-  balance.underlyingBorrowBalance = newUnderlyingBorrowBalance;
 
   // V2
   balance.scaledBorrowOnPool = newBalanceOnPool;
