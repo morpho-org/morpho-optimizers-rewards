567e9f2acc175188404efcc6a20c4816459c55a3
julien <julien@morpho.xyz>
2023-02-07 12:00:38 +0100

    feat(distributionV2): adapt initializers

--- subgraph/src/initializer.ts
+++ subgraph/src/initializer.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 
 import { Balance, Market, MarketEpochDistribution, User } from "../generated/schema";
 
@@ -29,12 +29,29 @@ export function getOrInitBalance(userAddress: Address, marketAddress: Address, c
     balance.blockNumber = 0;
     balance.market = market.id;
     balance.user = getOrInitUser(userAddress).id;
+
+    // V1 distribution mechanism properties
     balance.userBorrowIndex = market.borrowIndex;
     balance.userSupplyIndex = market.supplyIndex;
     balance.underlyingSupplyBalance = BigInt.zero();
     balance.underlyingBorrowBalance = BigInt.zero();
-    balance.unclaimedMorpho = BigInt.zero();
 
+    // V2 distribution mechanism properties
+    balance.scaledSupplyOnPool = BigInt.zero();
+    balance.scaledSupplyInP2P = BigInt.zero();
+
+    balance.scaledBorrowOnPool = BigInt.zero();
+    balance.scaledBorrowInP2P = BigInt.zero();
+
+    balance.userSupplyOnPoolIndex = market.lastPoolSupplyIndex;
+    balance.userSupplyInP2PIndex = market.lastP2PSupplyIndex;
+    balance.userBorrowOnPoolIndex = market.lastPoolBorrowIndex;
+    balance.userBorrowInP2PIndex = market.lastP2PBorrowIndex;
+
+    // Initialize the number of MORPHO distributed to zero.
+    balance.accumulatedMorpho = BigInt.zero();
+    balance.accumulatedBorrowMorpho = BigInt.zero();
+    balance.accumulatedSupplyMorpho = BigInt.zero();
     balance.save();
   }
 
@@ -47,16 +64,32 @@ export function getOrInitMarket(poolTokenAddress: Address, currentTimestamp: Big
   if (!market) {
     market = new Market(poolTokenAddress.toHexString());
     market.address = poolTokenAddress;
+
+    // V1 distribution mechanism properties
     market.borrowIndex = initialIndex;
     market.supplyIndex = initialIndex;
+
+    market.lastTotalSupply = BigInt.zero();
+    market.lastTotalBorrow = BigInt.zero();
+
+    // V2 distribution mechanism properties
+    market.scaledSupplyOnPool = BigInt.zero();
+    market.scaledSupplyInP2P = BigInt.zero();
+    market.scaledBorrowOnPool = BigInt.zero();
+    market.scaledBorrowInP2P = BigInt.zero();
+
+    market.poolSupplyIndex = BigInt.zero();
+    market.p2pSupplyIndex = BigInt.zero();
+
+    market.poolBorrowIndex = BigInt.zero();
+    market.p2pBorrowIndex = BigInt.zero();
+
     market.supplyUpdateBlockTimestamp = currentTimestamp;
     market.borrowUpdateBlockTimestamp = currentTimestamp;
     market.lastPoolSupplyIndex = WAD;
     market.lastP2PSupplyIndex = WAD;
     market.lastPoolBorrowIndex = WAD;
     market.lastP2PBorrowIndex = WAD;
-    market.lastTotalSupply = BigInt.zero();
-    market.lastTotalBorrow = BigInt.zero();
 
     market.save();
   }
@@ -87,6 +120,29 @@ export function getOrInitMarketEpoch(
     marketEpoch.isFinished = false;
     marketEpoch.timestamp = currentTimestamp;
     marketEpoch.speed = speed;
+    if (Bytes.fromUTF8(marketSide).equals(Bytes.fromUTF8("Supply"))) {
+      // V1
+      marketEpoch.index = market.supplyIndex;
+
+      // V2
+      marketEpoch.poolIndex = market.poolSupplyIndex;
+      marketEpoch.p2pIndex = market.p2pSupplyIndex;
+      marketEpoch.totalScaledP2P = market.scaledSupplyInP2P;
+      marketEpoch.totalScaledPool = market.scaledSupplyOnPool;
+    } else {
+      // V1
+      marketEpoch.index = market.borrowIndex;
+
+      // V2
+      marketEpoch.poolIndex = market.poolBorrowIndex;
+      marketEpoch.p2pIndex = market.p2pBorrowIndex;
+      marketEpoch.totalScaledP2P = market.scaledBorrowInP2P;
+      marketEpoch.totalScaledPool = market.scaledBorrowOnPool;
+    }
+
+    marketEpoch.poolSpeed = BigInt.zero();
+    marketEpoch.p2pSpeed = BigInt.zero();
+
     marketEpoch.save();
   }
 
