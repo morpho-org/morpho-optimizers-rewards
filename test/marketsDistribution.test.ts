b723d4e486d52b8b3710404aad66de493b3d06d4
julien <julien@morpho.xyz>
2023-02-07 12:20:47 +0100

    fix(tests): remove duplicated tests

--- test/marketsDistribution.test.ts
+++ test/marketsDistribution.test.ts
@@ -36,6 +36,7 @@ describe("Age 1 Epoch 2 approximation", () => {
     const newDistriUsers = Object.keys(newDistribution.proofs);
     Object.keys(onChainDistribution.proofs).forEach((onChainUser) => expect(newDistriUsers).toContain(onChainUser));
   });
+
   it("Should derive of 7e-18 only", () => {
     const newDistriUsers = Object.entries(newDistribution.proofs);
     Object.entries(onChainDistribution.proofs).forEach(([onChainUser, onChainDistrib]) => {
@@ -52,39 +53,12 @@ describe("Age 1 Epoch 2 approximation", () => {
     });
   });
 
-  describe("Age 1 Epoch 2 approximation", () => {
-    const onChainDistribution = require("../distribution/fromDeprecatedScript/proofs-2.json");
-    const newDistribution = require("../distribution/proofs/proofs-2.json");
-
-    it("Should have the same number of users", () =>
-      expect(Object.keys(onChainDistribution.proofs).length).toEqual(Object.keys(newDistribution.proofs).length));
-
-    it("Should contains the same users", () => {
-      const newDistriUsers = Object.keys(newDistribution.proofs);
-      Object.keys(onChainDistribution.proofs).forEach((onChainUser) => expect(newDistriUsers).toContain(onChainUser));
-    });
-    it("Should derive of 7e-18 only", () => {
-      const newDistriUsers = Object.entries(newDistribution.proofs);
-      Object.entries(onChainDistribution.proofs).forEach(([onChainUser, onChainDistrib]) => {
-        const [, compareTo] = newDistriUsers.find(([u]) => u === onChainUser)!;
-        // @ts-ignore unknown type
-        const compareToAmount = BigNumber.from(compareTo.amount);
-        // @ts-ignore unknown type
-        const onChainAmount = BigNumber.from(onChainDistrib.amount);
-        const dust = compareToAmount.gte(onChainAmount)
-          ? compareToAmount.sub(onChainAmount)
-          : onChainAmount.sub(compareToAmount);
-        // the max derivation from script version one and script version two is 7 * 1e-18 $MORPHO
-        expect(dust).toBnLte(7);
-      });
-    });
-    it("Should have the correct root on chain", async () => {
-      const provider = new providers.JsonRpcProvider(process.env.RPC_URL);
-      const age1epoch2 = ages[0].epochs[1];
-      const root = require("../distribution/fromDeprecatedScript/proofs-2.json").root;
-      const rewardsDistributor = RewardsDistributor__factory.connect(addresses.morphoDao.rewardsDistributor, provider);
-      const onchainRoot = await rewardsDistributor.currRoot({ blockTag: age1epoch2.finalBlock! + 10_000 });
-      expect(onchainRoot).toEqual(root);
-    });
+  it("Should have the correct root on chain", async () => {
+    const provider = new providers.JsonRpcProvider(process.env.RPC_URL);
+    const age1epoch2 = ages[0].epochs[1];
+    const root = require("../distribution/fromDeprecatedScript/proofs-2.json").root;
+    const rewardsDistributor = RewardsDistributor__factory.connect(addresses.morphoDao.rewardsDistributor, provider);
+    const onchainRoot = await rewardsDistributor.currRoot({ blockTag: age1epoch2.finalBlock! + 10_000 });
+    expect(onchainRoot).toEqual(root);
   });
 });
