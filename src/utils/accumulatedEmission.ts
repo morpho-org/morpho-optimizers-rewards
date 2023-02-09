511ae6f78510a1338000b3a04499a813103e486e
julien <julien@morpho.xyz>
2023-02-09 09:24:35 +0100

    test(marketData): Should distribute the correct number of tokens per market

--- src/utils/accumulatedEmission.ts
+++ src/utils/accumulatedEmission.ts
@@ -1,5 +1,6 @@
-import { ages } from "../ages";
-import { BigNumber } from "ethers";
+import { ages, allEpochs } from "../ages";
+import { BigNumber, constants } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
 
 /**
  * Returns the total distributed rewards for the previous epochs including the current one
@@ -16,3 +17,35 @@ export const getAccumulatedEmission = (epochId: string) => {
     .filter((e, index) => index <= currentEpochIndex)
     .reduce((acc, b) => acc.add(b.distributed), BigNumber.from(0));
 };
+
+/**
+ * Returns the total distributed rewards for the previous epochs including the current one on one given market
+ */
+export const getAccumulatedEmissionPerMarket = async (
+  market: string,
+  epochNumber: number
+): Promise<{ supply: BigNumber; borrow: BigNumber }> =>
+  Promise.all(
+    allEpochs
+      .filter((e) => e.number <= epochNumber)
+      .map(async ({ ageConfig, ...epoch }) => {
+        const { markets } = require(`../../distribution/${ageConfig.ageName}/${epoch.epochName}/marketsEmission.json`);
+        if (!markets[market])
+          return {
+            supply: constants.Zero,
+            borrow: constants.Zero,
+          };
+        return {
+          supply: parseUnits(markets[market]!.supply),
+          borrow: parseUnits(markets[market]!.borrow),
+        };
+      })
+  ).then((r) =>
+    r.reduce(
+      (acc, market) => ({
+        supply: acc.supply.add(market.supply),
+        borrow: acc.borrow.add(market.borrow),
+      }),
+      { supply: constants.Zero, borrow: constants.Zero }
+    )
+  );
