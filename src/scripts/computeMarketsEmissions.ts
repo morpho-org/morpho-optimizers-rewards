8f6effab86b4760a663c86e3d58730482c038b93
julien <julien@morpho.xyz>
2023-04-14 11:13:00 +0200

    feat(emissions): format total emission

--- src/scripts/computeMarketsEmissions.ts
+++ src/scripts/computeMarketsEmissions.ts
@@ -35,7 +35,7 @@ const computeMarketsEmissions = async (epochNumber?: number) => {
       age: marketsEmissions.age,
       epoch: marketsEmissions.epoch,
       markets: Object.keys(marketsEmissions.markets).length,
-      totalEmission: commify(formatUnits(marketsEmissions.totalEmission)),
+      totalEmission: commify(marketsEmissions.totalEmission),
       start: new Date(+marketsEmissions.parameters.initialTimestamp.toString() * 1000).toISOString(),
       end: new Date(+marketsEmissions.parameters.finalTimestamp.toString() * 1000).toISOString(),
     };
