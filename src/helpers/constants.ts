77673627e5dcca86e157702a8a79bcafa7473146
julien <julien@morpho.xyz>
2023-05-22 17:36:29 +0200

    fix(tests): add tests for 0 distributions for borrowers only if the market has rewards

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,3 +3,5 @@ import { BigNumber } from "ethers";
 export const WAD = BigNumber.from(10).pow(18);
 export const cFei = "0x7713dd9ca933848f6819f38b8352d9a15ea73f67".toLowerCase();
 export const aStEth = "0x1982b2f5814301d4e9a8b0201555376e62f82428".toLowerCase();
+
+export const wEth = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2".toLowerCase();
