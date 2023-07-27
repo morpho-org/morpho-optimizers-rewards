141182b77832f849c380e16fc0e2164395e261e6
julien <julien@morpho.xyz>
2023-07-27 17:54:11 +0200

    docs(merkletree): add documentation about merkle tree vulnerability

--- src/utils/merkleTree/merkleTree.ts
+++ src/utils/merkleTree/merkleTree.ts
@@ -4,7 +4,8 @@ export type Tree = ReturnType<typeof computeMerkleTree>;
 export const computeMerkleTree = (distribution: { address: string; accumulatedRewards: string }[]) => {
   const leaves = distribution.map(
     ({ address, accumulatedRewards }) =>
-      ethers.utils.solidityKeccak256(["address", "uint256"], [address, accumulatedRewards]) // 18 * 2 decimals
+      ethers.utils.solidityKeccak256(["address", "uint256"], [address, accumulatedRewards]) // 20 bytes + 32 bytes = 52 bytes, no collisions issues.
+    // REF: https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/master/contracts/utils/cryptography/MerkleProofUpgradeable.sol#L13
   );
   const merkleTree = new MerkleTree(leaves, ethers.utils.keccak256, {
     sortPairs: true,
